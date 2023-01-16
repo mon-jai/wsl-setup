983201ee98770bb0dcfbd3b9960eba8678285ab3
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-17 04:52:17 +0800

    Update setup-wsl.sh

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -47,11 +47,11 @@ curl --location "https://github.com/nushell/nushell/archive/refs/tags/${NU_VERSI
   tar -xz --strip-components 5 --transform "s/default_//" -C "$NU_CONFIG_DIRECTORY" "nushell-${NU_VERSION}/crates/nu-utils/src/sample_config/"
 rm --force "${NU_CONFIG_DIRECTORY}/sample_login.nu"
 
-printf "alias git = git.exe\n"                                                >> $NU_CONFIG_FILE
-printf "alias code = code-insiders\n"                                         >> $NU_CONFIG_FILE
-printf "alias node = node.exe\n"                                              >> $NU_CONFIG_FILE
-printf "alias docker = docker.exe\nalias docker-compose = docker-compose.exe" >> $NU_CONFIG_FILE
-sed -i 's/show_banner: true/show_banner: false/' $NU_CONFIG_FILE
+printf "alias git = git.exe\n"                                                >> "$NU_CONFIG_FILE"
+printf "alias code = code-insiders\n"                                         >> "$NU_CONFIG_FILE"
+printf "alias node = node.exe\n"                                              >> "$NU_CONFIG_FILE"
+printf "alias docker = docker.exe\nalias docker-compose = docker-compose.exe" >> "$NU_CONFIG_FILE"
+sed -i 's/show_banner: true/show_banner: false/'                                 "$NU_CONFIG_FILE"
 
-sed -i 's/def create_left_prompt/let home_directory_symlink_target = (wslpath (wslvar USERPROFILE) | str trim)\n\ndef create_left_prompt/' $NU_ENV_FILE
-sed -i 's/$path_segment/$path_segment | str replace --string $home_directory_symlink_target "~"/' $NU_ENV_FILE
+sed -i 's/def create_left_prompt/let home_directory_symlink_target = (wslpath (wslvar USERPROFILE) | str trim)\n\ndef create_left_prompt/' "$NU_ENV_FILE"
+sed -i 's/$path_segment/$path_segment | str replace --string $home_directory_symlink_target "~"/'                                          "$NU_ENV_FILE"
