b91d071d7b9cfd287037ec0deeda93315d620ae0
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-02-08 19:55:27 +0800

    Update setup-wsl.sh

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -58,7 +58,7 @@ sed -i 's/show_banner: true/show_banner: false/'                                
 perl -i -0pe 's/def create_left_prompt.*def create_right_prompt/def create_left_prompt [] {\n    let ansi_prefix = if (is-admin) { (ansi red_bold) } else { (ansi green_bold) }\n    let path = (\$env.PWD | str replace --string (wslpath (wslvar USERPROFILE) | str trim) \"~\" | str replace \"^\/mnt\" \"\" | str replace -a \"\/\" \" \/ \" | str trim)\n    \$ansi_prefix + \$path\n}\n\ndef create_right_prompt/s' $NU_ENV_FILE
 
 sed -i 's/let-env PROMPT_INDICATOR = { "〉" }/let-env PROMPT_INDICATOR = { " 〉" }/'                                  $NU_ENV_FILE
-printf "let-env PATH = (bash -c \$\"(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\\necho \$PATH;\")\n"          >> $NU_ENV_FILE
+printf "let-env PATH = (bash -c \$\"(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\\\\necho \$PATH;\")\n"        >> $NU_ENV_FILE
 printf "ls \$\"(which npm.ps1 | get 0.path | path dirname)/*.ps1\" | each {|it| get name | path basename }
 | each {|script_file| \$\"alias (\$script_file | str replace \".ps1\" \"\") = powershell.exe (\$script_file)\" }
 | str join \"\\\\n\" | save --force ~/.config/nushell/env-generated.nu\n"                                        >> $NU_ENV_FILE
