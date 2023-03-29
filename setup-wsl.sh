2ede4009226668365dc04f712e13cc9f6006a637
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-03-30 00:13:48 +0800

    Update setup-wsl.sh

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -86,7 +86,9 @@ powershell.exe -Command \"& { Get-Command -Type Application | ForEach-Object { \
   }
 )}
 | transpose key value | each { get value }
-| save --force ~/.config/nushell/env-generated.nu\n" >> $NU_ENV_FILE
+| save --force ~/.config/nushell/env-generated.nu
+
+" >> $NU_ENV_FILE
 sed -i 's/let-env PROMPT_INDICATOR = { "\(.\) " }/let-env PROMPT_INDICATOR = { " \1" }/' $NU_ENV_FILE
 printf "let-env PATH = (bash -c \$\"(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\\\\necho \$PATH;\")\n" >> $NU_ENV_FILE
 
